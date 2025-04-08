d20248d99a915106a963b9f6d3903c516531b99c
Noah Addy <noahaddy@users.noreply.github.com>
2025-04-08 10:12:20 -0700

    Adding charting component

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -6,6 +6,8 @@ import {
   InvoicesTable,
   LatestInvoiceRaw,
   Revenue,
+  
+
 } from './definitions';
 import { formatCurrency } from './utils';
 
@@ -13,15 +15,7 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 export async function fetchRevenue() {
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
-
-    // console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
